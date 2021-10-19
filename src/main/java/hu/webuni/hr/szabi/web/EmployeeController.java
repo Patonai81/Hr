f44e4e14f36a23e9648865b599f9b168bbe9b43c
Patonai Szabolcs <patonai.szabolcs@gmail.com>
2021-10-20 00:21:40 +0200

    Week3 házi part I

--- src/main/java/hu/webuni/hr/szabi/web/EmployeeController.java
+++ src/main/java/hu/webuni/hr/szabi/web/EmployeeController.java
@@ -4,9 +4,7 @@ package hu.webuni.hr.szabi.web;
 import hu.webuni.hr.szabi.model.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
@@ -24,19 +22,17 @@ public class EmployeeController {
 
     @GetMapping("/employeeManager")
     public String listEmployees(ModelMap model) {
-
        // employeeList.add(new Employee(1l, "Szabi", "Worker", 1000, LocalDateTime.of(2021, 4, 13, 8, 0, 0)));
 
         model.put("employeeList", employeeList);
         model.put("newEmployee", new Employee());
-        return "employeeList";
+        return "/employeeList";
     }
 
     @PostMapping("/employeeManager")
     public String addEmployee(Employee employee) {
         if (employee.getId()!= null && !employeeList.stream().anyMatch( e -> employee.getId()== e.getId()))
             employeeList.add(employee);
-
         return "redirect:employeeManager";
     }
 
@@ -48,14 +44,19 @@ public class EmployeeController {
         return modelAndView;
     }
 
+
     @PostMapping("/employeeManager/{id}")
     public String modifyEmployee(@PathVariable Integer id,Employee employee){
-
-        employeeList.removeIf( e -> e.getId()== employee.getId());
+     employeeList.removeIf( e -> e.getId()== employee.getId());
         employeeList.add(employee);
-
         return "redirect:/employeeManager/"+id;
 
+    }
+
+    @RequestMapping(value = "/employeeManager/{id}",method = {RequestMethod.DELETE})
+    public String deleteEmployee(@PathVariable long id) {
+        employeeList.removeIf( e -> e.getId()== id);
+        return "redirect:/employeeManager";
     }
 
 
