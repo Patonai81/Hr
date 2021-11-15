d0a1fc017e713926ff4d8609895a7c5a36096443
imre-gabor <imre.gabor@autsoft.hu>
2021-11-15 08:29:00 +0100

    use Junit5

--- src/test/java/hu/webuni/hr/szabi/CompanyServiceIT.java
+++ src/test/java/hu/webuni/hr/szabi/CompanyServiceIT.java
@@ -1,20 +1,17 @@
 package hu.webuni.hr.szabi;
 
-import hu.webuni.hr.szabi.exception.CompanyNotFoundException;
-import hu.webuni.hr.szabi.model.Company;
-import hu.webuni.hr.szabi.repository.CompanyRepository;
-import hu.webuni.hr.szabi.service.CompanyService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 
-import static org.springframework.test.annotation.DirtiesContext.ClassMode;
+import hu.webuni.hr.szabi.exception.CompanyNotFoundException;
+import hu.webuni.hr.szabi.model.Company;
+import hu.webuni.hr.szabi.repository.CompanyRepository;
+import hu.webuni.hr.szabi.service.CompanyService;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureTestDatabase
 @DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
