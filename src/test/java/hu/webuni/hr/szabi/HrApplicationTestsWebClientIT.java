b9e02cee3566db26b4978c85666fd53b2f5f2b0e
patonaiszabolcs <patonai.szabolcs@gmail.com>
2021-11-28 13:49:35 +0100

    FIX 70. Lecke

--- src/test/java/hu/webuni/hr/szabi/HrApplicationTestsWebClientIT.java
+++ src/test/java/hu/webuni/hr/szabi/HrApplicationTestsWebClientIT.java
@@ -1,13 +1,18 @@
 package hu.webuni.hr.szabi;
 
+import hu.webuni.hr.szabi.dto.CompanyDto;
 import hu.webuni.hr.szabi.dto.EmployeeDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 @SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -16,7 +21,7 @@ public class HrApplicationTestsWebClientIT {
     @Autowired
     WebTestClient webClient;
 
-    @Test
+   // @Test
     public void testReplaceEmployeePositive()  {
 
         EmployeeDto employee = new EmployeeDto("Béla", 2000, LocalDateTime.of(2018, 4, 13, 8, 0, 0));
@@ -28,8 +33,55 @@ public class HrApplicationTestsWebClientIT {
                 .expectStatus()
                 .isOk();
 
-    //    System.out.println(result);
+    }
+
+    @Test
+    public void testSaveEmployeeInCompany() {
+        CompanyDto company = getCompanies().get(0);
+        EmployeeDto teszt = new EmployeeDto("Teszt T", 100000, LocalDateTime.of(2020, Month.JULY, 29, 19, 30, 40));
+
+        webClient.post()
+                .uri("/api/companies/" + company.getCompanyId()+"/employeeRelated")
+                .body(Mono.just(teszt), EmployeeDto.class)
+                .exchange()
+                .expectStatus()
+                .isOk().expectBody(EmployeeDto.class).consumeWith(item -> {
+                    assertThat( item.getResponseBody().getEmployeeName().equals(teszt.getEmployeeName()));
+                });
+
 
     }
+
+
+
+
+        // helper methods ---------------------------------------------------------
+
+    public List<EmployeeDto> getEmployees() {
+        EntityExchangeResult<List<EmployeeDto>> result = webClient.get().uri(uriBuilder ->
+                        uriBuilder
+                                .path("/api/employee")
+                                .queryParam("full", "false")
+                                .build())
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(EmployeeDto.class)
+                .returnResult();
+        return result.getResponseBody();
+    }
+
+    public List<CompanyDto> getCompanies() {
+        EntityExchangeResult<List<CompanyDto>> result = webClient.get().uri(uriBuilder ->
+                        uriBuilder
+                                .path("/api/companies")
+                                .queryParam("full", "true")
+                                .build())
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(CompanyDto.class)
+                .returnResult();
+        return result.getResponseBody();
+    }
+
 
 }
